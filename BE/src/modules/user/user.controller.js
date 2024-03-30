b28f9d07697acee2dddf3a651a197fc6abba6f7f
yousef <yousefmahmoudmahmed@outlook.com>
2024-03-30 14:17:11 +0200

    generate the rest of CRUD operation (update , delete)

---
+++ BE/src/modules/user/user.controller.js
@@ -0,0 +1,19 @@
+import userModel from "../../../DB/model/User.model.js";
+import { asyncHandler } from "../../utils/errorHandling.js";
+
+export const update = asyncHandler(async (req, res, next) => {
+  // check if new email is taken by another user
+  const isNewEmail = await userModel.findOne({
+    _id: { $ne: req.user._id },
+    email: req.body.email,
+  });
+  if (isNewEmail) return next(new Error("Email is already taken"));
+
+  await req.user.updateOne(req.body); // update data into DB
+  return res.json({ messaeg: "Done" });
+});
+
+export const remove = asyncHandler(async (req, res, next) => {
+  await req.user.deleteOne();
+  return res.json({ messaeg: "Done" });
+});
--- BE/src/modules/user/user.router.js
+++ BE/src/modules/user/user.router.js
@@ -1,7 +1,9 @@
 import { Router } from "express";
-
+import * as usercontroller from "./user.controller.js";
+import auth from "../../middleware/auth.js";
 const router = Router();
 
 router.get("/", (req, res) => res.json({ mesasge: `${req.originalUrl} Page` }));
-
+router.put("/update", auth(), usercontroller.update);
+router.delete("/delete", auth(), usercontroller.remove);
 export default router;
