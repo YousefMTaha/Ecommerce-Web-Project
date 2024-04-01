7e65938f54e8c6d9529ba19c633d78a9d9a8fad2
yousef <yousefmahmoudmahmed@outlook.com>
2024-04-01 14:25:55 +0200

    implement important middlewares

---
+++ BE/src/middleware/isOwner.js
@@ -0,0 +1,19 @@
+import { StatusCodes } from "http-status-codes";
+import { ModifyError } from "../utils/classError.js";
+import { asyncHandler } from "../utils/errorHandling.js";
+
+export const isOwner = (model) => {
+  return asyncHandler(async (req, res, next) => {
+    const modelName = model.modelName.toLowerCase();
+
+    if (req[modelName].createdBy.toString() != req.user._id)
+      return next(
+        new ModifyError(
+          `Your are not the owner for this ${modelName}`,
+          StatusCodes.FORBIDDEN
+        )
+      );
+
+    next();
+  });
+};
