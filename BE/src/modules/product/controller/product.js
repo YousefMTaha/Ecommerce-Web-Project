c65c3c7d73cafecdc900743c77168d4fd7d6fca5
zeyad-1 <z01551153743@gmail.com>
2024-04-03 15:32:43 +0200

    implement CRUD operations

---
+++ BE/src/modules/product/controller/product.js
@@ -0,0 +1,45 @@
+import productModel from "../../../../DB/model/Product.model.js";
+import { asyncHandler } from "../../../utils/errorHandling.js";
+import * as validation from "../product.validation.js";
+
+export const addProduct = asyncHandler(async (req, res, next) => {
+    req.body.createdBy = await validation.validateUser(req);
+    req.body.category = await validation.validateCategory(req);
+    const product = await productModel.create(req.body);
+    return res.json({ message: "Product Added Successfully", product: product });
+});
+
+export const updateProduct = asyncHandler(async (req, res, next) => {
+    req.body.createdBy = await validation.validateUser(req);
+    req.body.category = await validation.validateCategory(req);
+    const product = await req.product.updateOne(req.body);
+    return res.json({ message: "Product Updated Successfully", product });
+});
+
+export const removeProduct = asyncHandler(async (req, res, next) => {
+    await req.product.deleteOne(req.body);
+    return res.json({ message: "Product Deleted Successfully" });
+});
+
+export const removeAllProducts = asyncHandler(async (req, res, next) => {
+    await productModel.deleteMany({})
+    return res.json({ message: "All Products Deleted Successfully" });
+});
+
+export const getProduct = asyncHandler(async (req, res, next) => {
+    const product = await productModel.findById(req.params.id).populate('category').populate('createdBy')
+    const modifiedProduct = product.toObject()
+    modifiedProduct.createdBy = product.createdBy.name
+    modifiedProduct.category = product.category.name
+    return res.json({ message: "Product Returned Successfully", modifiedProduct })
+});
+
+export const getAllProducts = asyncHandler(async (req, res, next) => {
+    const products = await productModel.find().populate('category').populate('createdBy')
+    const modifiedProducts = products.map(product => ({
+        ...product.toObject(),
+        createdBy: product.createdBy.name,
+        category: product.category.name
+    }));
+    return res.json({ message: "All Products Returned Successfully", modifiedProducts });
+});
