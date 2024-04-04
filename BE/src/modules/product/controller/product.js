d671c38211bab824af4f3f3d1642855bc98e2de6
yousef <yousefmahmoudmahmed@outlook.com>
2024-04-04 23:11:37 +0200

    modify bugs

--- BE/src/modules/product/controller/product.js
+++ BE/src/modules/product/controller/product.js
@@ -3,47 +3,77 @@ import { asyncHandler } from "../../../utils/errorHandling.js";
 import * as validation from "../product.validation.js";
 
 export const addProduct = asyncHandler(async (req, res, next) => {
-    req.body.createdBy = await validation.validateUser(req);
-    req.body.category = await validation.validateCategory(req);
-    req.body.quantity = await validation.validateNumber('quantity', req.body.quantity);
-    req.body.price = await validation.validateNumber('price', req.body.price);
-    const product = await productModel.create(req.body);
-    return res.json({ message: "Product Added Successfully", product: product });
+  req.body.createdBy = req.user._id;
+  req.body.subcategoryId = req.subcategory._id;
+  req.body.brandId = req.brand._id;
+  req.body.quantity = await validation.validateNumber(
+    "quantity",
+    req.body.quantity
+  );
+  req.body.price = await validation.validateNumber("price", req.body.price);
+  const product = await productModel.create(req.body);
+  return res.json({ message: "Product Added Successfully", product: product });
 });
 
 export const updateProduct = asyncHandler(async (req, res, next) => {
-    req.body.createdBy = await validation.validateUser(req);
-    req.body.category = await validation.validateCategory(req);
-    req.body.quantity = await validation.validateNumber('quantity', req.body.quantity);
-    req.body.price = await validation.validateNumber('price', req.body.price);
-    const product = await req.product.updateOne(req.body);
-    return res.json({ message: "Product Updated Successfully", modificationData: { acknowledged: product.acknowledged, productsFound: product.matchedCount, productsUpdated: product.modifiedCount } });
+  req.body.subcategoryId = !req.subcategory?._id
+    ? req.product.subcategoryId
+    : req.subcategory._id;
+
+  req.body.brandId = req.brand?._id ? req.brand._id : req.product.brandId;
+  req.body.quantity = await validation.validateNumber(
+    "quantity",
+    req.body.quantity
+  );
+  // productModel.findOneAndUpdate({},{},{new:true})
+  req.body.price = await validation.validateNumber("price", req.body.price);
+  const product = await req.product.updateOne(req.body);
+  return res.json({
+    message: "Product Updated Successfully",
+    modificationData: {
+      acknowledged: product.acknowledged,
+      productsFound: product.matchedCount,
+      productsUpdated: product.modifiedCount,
+    },
+  });
 });
 
 export const removeProduct = asyncHandler(async (req, res, next) => {
-    await req.product.deleteOne(req.body);
-    return res.json({ message: "Product Deleted Successfully" });
+  await req.product.deleteOne();
+  return res.json({ message: "Product Deleted Successfully" });
 });
 
-export const removeAllProducts = asyncHandler(async (req, res, next) => {
-    await productModel.deleteMany({})
-    return res.json({ message: "All Products Deleted Successfully" });
-});
+// export const removeAllProducts = asyncHandler(async (req, res, next) => {
+//   await productModel.deleteMany({});
+//   return res.json({ message: "All Products Deleted Successfully" });
+// });
 
 export const getProduct = asyncHandler(async (req, res, next) => {
-    const product = await productModel.findById(req.params.id).populate('category').populate('createdBy')
-    const modifiedProduct = product.toObject()
-    modifiedProduct.createdBy = product.createdBy.name
-    modifiedProduct.category = product.category.name
-    return res.json({ message: "Product Returned Successfully", product: modifiedProduct })
+  const product = await productModel
+    .findById(req.params.id)
+    .populate("subcategoryId")
+    .populate("createdById");
+  const modifiedProduct = product.toObject();
+  modifiedProduct.createdById = product.createdById.name;
+  modifiedProduct.subcategoryId = product.subcategoryId.name;
+  return res.json({
+    message: "Product Returned Successfully",
+    product: modifiedProduct,
+  });
 });
 
 export const getAllProducts = asyncHandler(async (req, res, next) => {
-    const products = await productModel.find().populate('category').populate('createdBy')
-    const modifiedProducts = products.map(product => ({
-        ...product.toObject(),
-        createdBy: product.createdBy.name,
-        category: product.category.name
-    }));
-    return res.json({ message: "All Products Returned Successfully", products: modifiedProducts });
+  const products = await productModel
+    .find()
+    .populate("subcategoryId")
+    .populate("createdById");
+  const modifiedProducts = products.map((product) => ({
+    ...product.toObject(),
+    createdBy: product.createdById.name,
+    category: product.subcategoryId.name,
+  }));
+  return res.json({
+    message: "All Products Returned Successfully",
+    products: modifiedProducts,
+  });
 });
--- BE/src/modules/product/product.router.js
+++ BE/src/modules/product/product.router.js
@@ -5,20 +5,66 @@ import productModel from "../../../DB/model/Product.model.js";
 import * as productController from "./controller/product.js";
 import { isExist } from "../../middleware/isExist.js";
 import { isOwner } from "../../middleware/isOwner.js";
+import subcategoryModel from "../../../DB/model/Subcategory.model.js";
+import { reqDataForms, uniqueFields } from "../../utils/systemConstants.js";
+import brandModel from "../../../DB/model/Brand.model.js";
 
 const router = Router();
 
-router.get("/", auth(), productController.getAllProducts)
+router.get("/", auth(), productController.getAllProducts);
 
-router.get("/:id", auth(), isExist(productModel), isOwner(productModel), productController.getProduct)
+router.get(
+  "/:_id",
+  auth(),
+  isExist({ model: productModel }),
+  // isOwner(productModel),
+  productController.getProduct
+);
 
-router.post("/", auth(), isNotExist({ model: productModel, searchData: "name" }), productController.addProduct)
+router.post(
+  "/",
+  auth(),
+  isExist({
+    model: subcategoryModel,
+    dataFrom: reqDataForms.body,
+    searchData: uniqueFields.subcategoryId,
+  }),
+  isExist({
+    model: brandModel,
+    dataFrom: reqDataForms.body,
+    searchData: uniqueFields.brandId,
+  }),
+  isNotExist({ model: productModel, searchData: "name" }),
+  productController.addProduct
+);
 
-router.delete("/", auth(), productController.removeAllProducts)
+// router.delete("/", auth(), productController.removeAllProducts);
 
-router.delete("/:id", auth(), isExist(productModel), isOwner(productModel), productController.removeProduct)
+router.delete(
+  "/:_id",
+  auth(),
+  isExist({ model: productModel }),
+  isOwner(productModel),
+  productController.removeProduct
+);
 
-router.put("/:id", auth(), isExist(productModel), isOwner(productModel), productController.updateProduct)
-
+router.put(
+  "/:_id",
+  auth(),
+  isExist({ model: productModel }),
+  isOwner(productModel),
+  isNotExist({ model: productModel, searchData: "name" }),
+  isExist({
+    model: subcategoryModel,
+    dataFrom: reqDataForms.body,
+    searchData: uniqueFields.subcategoryId,
+  }),
+  isExist({
+    model: brandModel,
+    dataFrom: reqDataForms.body,
+    searchData: uniqueFields.brandId,
+  }),
+  productController.updateProduct
+);
 
 export default router;
