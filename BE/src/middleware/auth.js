4eb17831376567b5286888cbc76108dc9bcdc58d
yousef <yousefmahmoudmahmed@outlook.com>
2024-04-04 22:38:11 +0200

    add status code to auth

--- BE/src/middleware/auth.js
+++ BE/src/middleware/auth.js
@@ -2,39 +2,57 @@ import jwt from "jsonwebtoken";
 import userModel from "../../DB/model/User.model.js";
 import { userRoles, userStatus } from "../utils/systemConstants.js";
 import { asyncHandler } from "../utils/errorHandling.js";
+import { ModifyError } from "../utils/classError.js";
+import { StatusCodes } from "http-status-codes";
 
 const auth = (roles = Object.values(userRoles)) => {
   return asyncHandler(async (req, res, next) => {
     // constract token from headers
     let { token } = req.headers;
-    if (!token) return next(new Error("token must be entered"));
+    if (!token)
+      return next(
+        new ModifyError("token must be entered", StatusCodes.BAD_REQUEST)
+      );
 
     // validate the beaerer key
     if (!token.startsWith(process.env.BEARER_KEY))
-      return next(new Error("invalid bearer key"));
+      return next(
+        new ModifyError("invalid bearer key", StatusCodes.BAD_REQUEST)
+      );
 
     // split the bearer key from original token =>  "bearer_token" => ["","token"]
     token = token.split(process.env.BEARER_KEY)[1];
 
     // constract id from token
     const { id } = jwt.verify(token, process.env.TOKEN_SIGNATURE);
-    if (!id) return next(new Error("invalid payload"));
+    if (!id)
+      return next(new ModifyError("invalid payload", StatusCodes.BAD_REQUEST));
 
     // check if the user exist or not
     const user = await userModel.findById(id);
-    if (!user) return next(new Error("user id doesn't exist"));
+    if (!user)
+      return next(new Error("user id doesn't exist", StatusCodes.NOT_FOUND));
 
     // check the user authorization
     if (!roles.includes(user.role))
-      return next(new Error("You are not authorized"));
+      return next(
+        new Error("You are not authorized", StatusCodes.UNAUTHORIZED)
+      );
 
     // check if user blocked
     if (user.status == userStatus.Blocked)
-      return next(new Error("Your account has been blocked, contact us"));
+      return next(
+        new Error(
+          "Your account has been blocked, contact us",
+          StatusCodes.FORBIDDEN
+        )
+      );
 
     // check if user offline
     if (user.status == userStatus.Offline)
-      return next(new Error("You need to login again"));
+      return next(
+        new ModifyError("You need to login again", StatusCodes.BAD_REQUEST)
+      );
 
     // add the user object to the request object so it can be accessed throw the other middlewares
     req.user = user;
