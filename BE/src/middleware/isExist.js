835887a10e2b8f2e6e688c6b78b2700c9b82c493
yousef <yousefmahmoudmahmed@outlook.com>
2024-04-04 23:11:29 +0200

    modify some bugs

--- BE/src/middleware/isExist.js
+++ BE/src/middleware/isExist.js
@@ -12,16 +12,19 @@ export const isExist = ({
     // construct model name from model
     const modelName = model.modelName.toLowerCase();
 
-    // check the id exist ?
-    const isModelExist = await model.findById(req[dataFrom][searchData]);
-    if (!isModelExist)
-      return next(
-        new ModifyError(
-          `${modelName} ${searchData} doesn't exist`,
-          StatusCodes.NOT_FOUND
-        )
-      );
+    let isModelExist;
 
+    if (req[dataFrom][searchData]) {
+      // check the id exist ?
+      isModelExist = await model.findById(req[dataFrom][searchData]);
+      if (!isModelExist)
+        return next(
+          new ModifyError(
+            `${modelName} ${searchData} doesn't exist`,
+            StatusCodes.NOT_FOUND
+          )
+        );
+    }
     // add model data to the request object to use it throw other middlewares
     req[modelName] = isModelExist;
     next();
