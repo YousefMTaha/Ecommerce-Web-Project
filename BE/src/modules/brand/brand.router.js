ab2188335c8e8683c88ae8e0bd841a3f672d6912
yousef <yousefmahmoudmahmed@outlook.com>
2024-04-04 20:22:15 +0200

    implement brand api (add)

--- BE/src/modules/brand/brand.router.js
+++ BE/src/modules/brand/brand.router.js
@@ -1,7 +1,17 @@
 import { Router } from "express";
-
+import auth from "../../middleware/auth.js";
+import { isNotExist } from "../../middleware/isNotExist.js";
+import brandModel from "../../../DB/model/Brand.model.js";
+import { uniqueFields } from "../../utils/systemConstants.js";
+import * as brandController from './brand.controller.js'
 const router = Router();
 
-router.get("/", (req, res) => res.json({ mesasge: `${req.originalUrl} Page` }));
-
+router
+  .route("/")
+  .get((req, res) => res.json({ mesasge: `${req.originalUrl} Page` }))
+  .post(
+    auth(),
+    isNotExist({ model: brandModel, searchData: uniqueFields.name }),
+    brandController.add
+  );
 export default router;
