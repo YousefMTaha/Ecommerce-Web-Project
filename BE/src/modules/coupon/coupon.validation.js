bacea99e25e47dea817a43027b1242cb2f214027
zeyad-1 <z01551153743@gmail.com>
2024-04-08 15:35:46 +0200

    create coupon and user validity functions

--- BE/src/modules/coupon/coupon.validation.js
+++ BE/src/modules/coupon/coupon.validation.js
@@ -1,12 +1,10 @@
 import { StatusCodes } from "http-status-codes";
-import couponModel from "../../../DB/model/Coupon.model.js";
 import { ModifyError } from "../../utils/classError.js";
 import { asyncHandler } from "../../utils/errorHandling.js";
 
 export const couponValidity = () => {
     return asyncHandler(async (req, res, next) => {
-        const coupon = await couponModel.findOne(req.coupon.code)
-        if (coupon.usages <= 0) {
+        if (req.coupon.usages <= 0) {
             return next(new ModifyError(`coupon is not active`, StatusCodes.FORBIDDEN))
         }
         next();
@@ -15,8 +13,10 @@ export const couponValidity = () => {
 
 export const userValidity = () => {
     return asyncHandler(async (req, res, next) => {
-        const coupon = await couponModel.findOne(req.coupon.code)
-        if (coupon.users[req.user._id] >= coupon.limit) {
+        if (!req.coupon.users[req.user._id]) {
+            req.coupon.users[req.user._id] = 0
+        }
+        if (req.coupon.users[req.user._id] >= req.coupon.limit) {
             return next(new ModifyError(`user reach his limit`, StatusCodes.FORBIDDEN))
         }
         next();
