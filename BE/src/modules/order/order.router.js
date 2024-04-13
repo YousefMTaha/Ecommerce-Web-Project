f794ca3f32d26437140ed8d2372a83ae23ba9088
yousef <yousefmahmoudmahmed@outlook.com>
2024-04-13 21:59:06 +0200

    implement order router and stripe router

--- BE/src/modules/order/order.router.js
+++ BE/src/modules/order/order.router.js
@@ -1,7 +1,37 @@
 import { Router } from "express";
+import auth from "../../middleware/auth.js";
+import * as orderMiddleware from "./order.middleware.js";
+import * as orderController from "./order.controller.js";
+import { isExist } from "../../middleware/isExist.js";
+import orderModel from "../../../DB/model/Order.model.js";
+import { reqDataForms, uniqueFields } from "../../utils/systemConstants.js";
+import { isOwner } from "../../middleware/isOwner.js";
 
 const router = Router();
 
-router.get("/", (req, res) => res.json({ mesasge: `${req.originalUrl} Page` }));
+router.get("/", (req, res) =>
+  res.status(200).json({ mesasge: `${req.originalUrl} Page` })
+);
+// Generate order
+router.post(
+  "/",
+  auth(),
+  orderMiddleware.isProductsOrder,
+  orderMiddleware.orderCard,
+  orderMiddleware.orderCash,
+  orderController.create
+);
+
+// payment gateway
+router.post("/webhook", orderController.webhook);
+
+router.put(
+  "/:_id",
+  auth(),
+  isExist({ model: orderModel }),
+  isOwner(orderModel),
+  orderMiddleware.refund,
+  orderController.cancel
+);
 
 export default router;
