268733189303b0fdb570216a47432a6f30b4dbd6
yousef <yousefmahmoudmahmed@outlook.com>
2024-04-13 21:55:59 +0200

    implement  (update , delete)

--- BE/src/modules/brand/brand.controller.js
+++ BE/src/modules/brand/brand.controller.js
@@ -3,8 +3,16 @@ import { asyncHandler } from "../../utils/errorHandling.js";
 
 export const add = asyncHandler(async (req, res, next) => {
   req.body.createdBy = req.user._id;
-  return res.json({
-    message: "Done",
-    brand: await brandModel.create(req.body),
-  });
+  req.brand = await brandModel.create(req.body);
+  return next();
+});
+
+export const update = asyncHandler(async (req, res, next) => {
+  await req.brand.updateOne(req.body);
+  return res.status(200).json({ message: "Done" });
+});
+
+export const remove = asyncHandler(async (req, res, next) => {
+  await req.brand.deleteOne();
+  return res.status(200).json({ message: "Done" });
 });
