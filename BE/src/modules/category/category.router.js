4483c2eebc8d898118c1836fdce332622f12113b
yousef <yousefmahmoudmahmed@outlook.com>
2024-04-13 21:56:46 +0200

    add image features

--- BE/src/modules/category/category.router.js
+++ BE/src/modules/category/category.router.js
@@ -5,29 +5,42 @@ import categoryModel from "../../../DB/model/Category.model.js";
 import { isNotExist } from "../../middleware/isNotExist.js";
 import { isOwner } from "../../middleware/isOwner.js";
 import { isExist } from "../../middleware/isExist.js";
+import { fileUpload } from "../../utils/multer.js";
+import {
+  deleteImage,
+  updateImage,
+  uploadImage,
+} from "../../middleware/uploadImage.js";
+import { fileValidation, uniqueFields } from "../../utils/systemConstants.js";
 const router = Router();
 router
   .route("/")
   .get(categoryController.getAllData)
   .post(
     auth(),
-    isNotExist({ model: categoryModel, searchData: "name" }),
-    categoryController.add
+    fileUpload(fileValidation.image).single("img"),
+    isNotExist({ model: categoryModel, searchData: uniqueFields.name }),
+    categoryController.add,
+    uploadImage(categoryModel)
   );
+
 router
   .route("/:id")
   .get(categoryController.getData)
   .put(
     auth(),
+    fileUpload(fileValidation.image).single("img"),
     isExist({ model: categoryModel }),
     isOwner(categoryModel),
-    isNotExist({ model: categoryModel, searchData: "name" }),
+    isNotExist({ model: categoryModel, searchData: uniqueFields.name }),
+    updateImage(categoryModel),
     categoryController.update
   )
   .delete(
     auth(),
     isExist({ model: categoryModel }),
     isOwner(categoryModel),
+    deleteImage(categoryModel),
     categoryController.remove
   );
 export default router;
