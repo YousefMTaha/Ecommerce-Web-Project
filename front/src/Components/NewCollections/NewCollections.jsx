1c2d9fdf7f4c9714cb7da219f4ae1a80ba1f2aaf
mahmoud <mahmoudhassan10191019@gmail.com>
2024-05-10 20:47:12 +0300

    fetch products

--- front/src/Components/NewCollections/NewCollections.jsx
+++ front/src/Components/NewCollections/NewCollections.jsx
@@ -1,26 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./NewCollections.css";
-import newCollection from "../Assets/new_collections";
 import Item from "../Items/item";
 
 const NewCollections = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const res = await fetch('http://localhost:3000/product');
+        const data = await res.json();
+        setProducts(data.Product); // Access the correct key "Product" instead of "products"
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
   return (
     <div className="newCollections">
       <h1>NEW COLLECTIONS</h1>
       <hr />
       <div className="collections">
-        {newCollection.map((item, i) => {
-          return (
-            <Item
-              key={i}
-              id={item.id}
-              name={item.name}
-              image={item.image}
-              newPrice={item.new_price}
-              oldPrice={item.old_price}
-            />
-          );
-        })}
+        {products.map((item, i) => (
+          <Item
+            key={i}
+            id={item.id}
+            name={item.name}
+            image={item.images[0].secure_url} // Access the first image, or revise according to your requirement
+            newPrice={item.price} // Verify if newPrice should be price
+            stock={item.stock} // Verify if oldPrice should be stock
+          />
+        ))}
       </div>
     </div>
   );
