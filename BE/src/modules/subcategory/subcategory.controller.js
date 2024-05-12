aae90d3a46cf730b682977613eb92e4f1b017fad
yousef <yousefmahmoudmahmed@outlook.com>
2024-05-12 18:28:05 +0300

    create subcategories api

--- BE/src/modules/subcategory/subcategory.controller.js
+++ BE/src/modules/subcategory/subcategory.controller.js
@@ -1,4 +1,6 @@
+import { StatusCodes } from "http-status-codes";
 import subcategoryModel from "../../../DB/model/Subcategory.model.js";
+import { ModifyError } from "../../utils/classError.js";
 import { asyncHandler } from "../../utils/errorHandling.js";
 
 export const add = asyncHandler(async (req, res, next) => {
@@ -16,4 +18,13 @@ export const remove = asyncHandler(async (req, res, next) => {
 export const update = asyncHandler(async (req, res, next) => {
   await req.subcategory.updateOne(req.body);
   return res.status(200).json({ message: "done" });
+});
+
+export const getCategorySubcategory = asyncHandler(async (req, res, next) => {
+  const cagtegories = await subcategoryModel.find({
+    categoryId: req.params._id,
+  });
+  return cagtegories.length
+    ? res.status(200).json({ message: "done", cagtegories })
+    : next(new ModifyError("no sub catgeory found", StatusCodes.NOT_FOUND));
 });
